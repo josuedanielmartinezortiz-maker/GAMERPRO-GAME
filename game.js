7889ec7ce535794344ec91424546ee3e2ba3abef
josuedanielmartinezortiz-maker <josuedanielmartinezortiz@gmail.com>
2026-09-03 16:39:42 -0600

    Fix event listener syntax and update image path

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 const playButton = document.getElementById("playButton");
 const game = document.getElementById("game");
 
-playButton.addEventListener("click", () => {
+playButton.addEventListener("click", function () {
     game.innerHTML = `
         <div class="scene">
-            <img src="IMG-20260903-WA2716.jpg" alt="Amanecer en la granja">
+            <img src="./IMG-20260903-WA2716.jpg" alt="Amanecer en la granja">
             <p class="dialogo">Pío pío...</p>
         </div>
     `;
